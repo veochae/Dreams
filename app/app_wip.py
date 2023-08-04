c85b7bf7b8cd7a555fca8582f0f5527671f490d8
Veo Chae <veochae@gmail.com>
2023-08-04 06:54:29 -0400

    push

--- app/app_wip.py
+++ app/app_wip.py
@@ -865,7 +865,7 @@ def summary_continue():
 
     # dream = pd.DataFrame(st.session_state['clean_text']).iloc[st.session_state['row_n'],:]
     dream  = st.text_input("somethingf")
-    summary = summarize_dream(x+ "\n\nTl;dr")
+    summary = summarize_dream(dream+ "\n\nTl;dr")
     continuation = summarize_dream("What happend after this story from the storyteller's perspective? \n" + dream + "\n [insert]")
 
     st.write("Dream Summary")
