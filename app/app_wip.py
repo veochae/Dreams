2d8a94899ac7bd7c5a86016517917c3929b409ca
Veo Chae <veochae@gmail.com>
2023-08-23 15:30:31 -0400

    c

--- app/app_wip.py
+++ app/app_wip.py
@@ -148,12 +148,12 @@ def reddit_data(time_wanted, headers):
 
 def introduction():
     l1 = load_lottieurl("https://lottie.host/0e7f8667-876c-4470-9509-1938d325dbc7/1z7IT8acx2.json")
-    col1, col2 = st.columns([3,1])
+    col1, col2 = st.columns([4,1])
     with col1:
         st.title("Analyzing Dreams using NLP")
 
     with col2:
-        st_lottie(l1, key = "l1", height = 50, width = 100)
+        st_lottie(l1, key = "l1", height = 100, width = 200)
 
 ########################################################################################
 #############################       data collection page      ##########################
