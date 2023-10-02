4161507dd62644305c0c13e88c42eb968fc2dbe1
Veo Chae <veochae@gmail.com>
2023-10-01 23:27:58 -0400

    asdf

--- app/app_wip.py
+++ app/app_wip.py
@@ -125,7 +125,7 @@ def reddit_data(time_wanted, headers):
                 st.write("Data Collection Target Reached")
                 st.write(f'{len(df)} rows collected')
                 st.write(f'latest subreddit date: {datetime.fromtimestamp(latest)}')
-                df.text = list(map(lambda x: profanity.censor(x),df.text))
+                # df.text = list(map(lambda x: profanity.censor(x),df.text))
                 return df, res.json()['data']['children'][1]
 
     else: 
