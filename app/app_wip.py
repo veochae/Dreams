cd8719d8be09a2321602c363af8ba4b83745c4ed
Veo Chae <veochae@gmail.com>
2023-10-01 21:01:46 -0400

    fix

--- app/app_wip.py
+++ app/app_wip.py
@@ -304,7 +304,7 @@ def data_cleaning():
                 df, semi = preprocess(st.session_state['reddit'])
 
                 st.info("Type in Keyword you would like to see in the Dream")
-                st.session_state['keyword'] = st.text_input()
+                st.session_state['keyword'] = st.text_input("Keyword:")
                 filtered = df[df['text'].str.contains(st.session_state['keyword'])]
                 
                 if "keyword" in st.session_state.keys():
