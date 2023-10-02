bfbe20ebf397ff55d15f57a163c7916c50870b48
Veo Chae <veochae@gmail.com>
2023-10-01 23:46:36 -0400

    asdf

--- app/app_wip.py
+++ app/app_wip.py
@@ -924,8 +924,9 @@ def summary_continue():
         with st.form("asdf"):
             st.header("Original Text")
             try:
-                dream = st.session_state['semi']['text'][st.session_state['row_n']]
-                st.write(dream)
+                with st.expander(f"Click Here to View the Selected Dream "):
+                    dream = st.session_state['semi']['text'][st.session_state['row_n']]
+                    st.write(dream)
             except:
                 pass
             dream_submit = st.form_submit_button("Proceed to Summarization and Continuation") 
