7574f1f995209965ea3913c35b6e1b3f53df844b
Veo Chae <veochae@gmail.com>
2023-10-30 16:30:31 -0400

    asdf

--- app/app_wip.py
+++ app/app_wip.py
@@ -643,6 +643,9 @@ def data_cleaning():
                             
                             tf_idf_mean = st.session_state['tf_idf_df'].describe().iloc[1,:].tolist()
                             t_f = [False if z < np.percentile(tf_idf_mean,80) else True for z in tf_idf_mean]
+                            st.write(np.percentile(tf_idf_mean,99))
+                            st.write(np.percentile(tf_idf_mean,95))
+                            st.write(np.percentile(tf_idf_mean,90))
                             st.write(np.percentile(tf_idf_mean,80))
                             st.write(np.mean(tf_idf_mean))
 
