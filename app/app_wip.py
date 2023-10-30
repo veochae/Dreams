78b0f68a319451344ae7abd5161472deb5581496
Veo Chae <veochae@gmail.com>
2023-10-30 16:47:29 -0400

    asdf

--- app/app_wip.py
+++ app/app_wip.py
@@ -642,7 +642,6 @@ def data_cleaning():
 
                             
                             tf_idf_mean = st.session_state['tf_idf_df'].describe().iloc[1,:].tolist()
-                            t_f = [False if z < st.write(np.mean(np.nonzero(np.array(tf_idf_mean)))) else True for z in tf_idf_mean]
                             st.write(np.percentile(np.nonzero(np.array(tf_idf_mean)),99))
                             st.write(np.percentile(np.nonzero(np.array(tf_idf_mean)),95))
                             st.write(np.percentile(np.nonzero(np.array(tf_idf_mean)),90))
@@ -650,6 +649,9 @@ def data_cleaning():
                             st.write(np.percentile(np.min(np.array(tf_idf_mean)),80))
                             st.write(np.percentile(np.max(np.array(tf_idf_mean)),80))
                             st.write(np.mean(np.nonzero(np.array(tf_idf_mean))))
+
+                            t_f = [False if z < st.write(np.mean(np.nonzero(np.array(tf_idf_mean)))) else True for z in tf_idf_mean]
+
 
                             not_words = [j for e,j in enumerate(st.session_state['tf_idf_df'].columns) if t_f[e] == False]
                             words = [k for k in st.session_state['clean_text'] if k not in not_words]
