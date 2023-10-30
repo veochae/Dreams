7e3c211a66440b7673dd5a04619f316baa6d6096
Veo Chae <veochae@gmail.com>
2023-10-30 16:19:34 -0400

    asdf

--- app/app_wip.py
+++ app/app_wip.py
@@ -635,10 +635,8 @@ def data_cleaning():
                                 return pd.DataFrame(tf_idf_li)
 
                             st.session_state['tf_idf_df'] = main(corpus, tokenized)
-                            st.dataframe(st.session_state['tf_idf_df'])
-
                             tf_idf_mean = st.session_state['tf_idf_df'].describe().iloc[1,:].tolist()
-                            t_f = [True if z < np.mean(tf_idf_mean) else False for z in tf_idf_mean]
+                            t_f = [False if z < np.mean(tf_idf_mean) else True for z in tf_idf_mean]
 
                             not_words = [j for e,j in enumerate(st.session_state['tf_idf_df'].columns) if t_f[e] == True]
                             words = [k for k in st.session_state['clean_text'] if k not in not_words]
