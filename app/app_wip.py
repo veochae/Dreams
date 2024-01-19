8061941c99b5ebdeefb10f903e8fc24f69372dc2
Veo Chae <veochae@gmail.com>
2024-01-19 05:19:17 -0500

    asdf

--- app/app_wip.py
+++ app/app_wip.py
@@ -935,61 +935,61 @@ def set_up_openai():
 ######################################################################################## 
 def sentiment_analysis():
     st.title("Sentiment Analysis")
-    try:
-        st.write("While in the earlier days of ecommerce online sellers would be satisfied with any reviews of the products and services they were offering, in recent years companies have shifted into asking for more detailed reviews. Why? Because using Sentiment Analysis companies can learn how the customers feel about their products, and the more detailed the review the higher the chances of accurately uncovering customer feelings!")
-        st.write("In the earlier days of the Sentiment Analysis, it was quite simple. One would classify a piece of text as positive, negative, or neutral. We won't dive into too much detail about how that has been done, but if you are curious, checkout this [link](https://huggingface.co/blog/sentiment-analysis-python)! Coming back to the modern days, Sentiment Analysis has evolved into something more specific and granular: Emotion Analysis")
-        st.write("Instead of uncovering whether the customers' reactions were positive or negative, one starts to look at multiple (and more granular) feelings such as: fear, joy, happiness, surprise, love, anger, sadness, etc.")
-        st.write("The pretrained model that we use for the purpose of this exercise is from Hugging Face – a popular platform for [NLP](https://huggingface.co/). In particular, using over 100,000 labeled tweets as its training data, neural networks were trained to recognize different speech patterns that lend themselves to various feelings and emotions. Having learned about human emotion based on training data, the model can now be applied to any piece of text – including text on reported dreams – to predict emotion!")
-        st.write("So, without further ado, let's uncover the emotions present in the dream that you selected at the outset!")
+    # try:
+    st.write("While in the earlier days of ecommerce online sellers would be satisfied with any reviews of the products and services they were offering, in recent years companies have shifted into asking for more detailed reviews. Why? Because using Sentiment Analysis companies can learn how the customers feel about their products, and the more detailed the review the higher the chances of accurately uncovering customer feelings!")
+    st.write("In the earlier days of the Sentiment Analysis, it was quite simple. One would classify a piece of text as positive, negative, or neutral. We won't dive into too much detail about how that has been done, but if you are curious, checkout this [link](https://huggingface.co/blog/sentiment-analysis-python)! Coming back to the modern days, Sentiment Analysis has evolved into something more specific and granular: Emotion Analysis")
+    st.write("Instead of uncovering whether the customers' reactions were positive or negative, one starts to look at multiple (and more granular) feelings such as: fear, joy, happiness, surprise, love, anger, sadness, etc.")
+    st.write("The pretrained model that we use for the purpose of this exercise is from Hugging Face – a popular platform for [NLP](https://huggingface.co/). In particular, using over 100,000 labeled tweets as its training data, neural networks were trained to recognize different speech patterns that lend themselves to various feelings and emotions. Having learned about human emotion based on training data, the model can now be applied to any piece of text – including text on reported dreams – to predict emotion!")
+    st.write("So, without further ado, let's uncover the emotions present in the dream that you selected at the outset!")
 
-        with st.form("sentiment_analysis"):
-            st.info(f"Chosen Dream: Dream {st.session_state['row_n']}",icon="ℹ️")
-            with st.expander(f"Click Here to View the Selected Dream "):
-                dream = st.session_state['semi']['text'][st.session_state['row_n']]
-                st.write(dream)        
-            submitted_sentiment = st.form_submit_button("Let's Begin!")   
-        
-        if submitted_sentiment:
-        # try:
-            openai.api_key = st.session_state['openai_key']
+    with st.form("sentiment_analysis"):
+        st.info(f"Chosen Dream: Dream {st.session_state['row_n']}",icon="ℹ️")
+        with st.expander(f"Click Here to View the Selected Dream "):
+            dream = st.session_state['semi']['text'][st.session_state['row_n']]
+            st.write(dream)        
+        submitted_sentiment = st.form_submit_button("Let's Begin!")   
+    
+    if submitted_sentiment:
+    # try:
+        openai.api_key = st.session_state['openai_key']
 
-            # try:     
-            summary = summarize_dream("Summarize this dream to less than 280 words from the storyteller's perspective \n" + "Dream: " + dream, length = 280)
-            # except Exception as e:
-            #     st.warning("This Error is either: 1. Do not have enough API balance 2. Not the correct API Key")
-            #     print(e)
+        # try:     
+        summary = summarize_dream("Summarize this dream to less than 280 words from the storyteller's perspective \n" + "Dream: " + dream, length = 280)
+        # except Exception as e:
+        #     st.warning("This Error is either: 1. Do not have enough API balance 2. Not the correct API Key")
+        #     print(e)
 
-            classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', top_k = None)
-            prediction = classifier(summary)
-            emotion = [x['label'] for x in prediction[0]]
-            score = [y['score'] for y in prediction[0]]
+        classifier = pipeline("text-classification",model='bhadresh-savani/distilbert-base-uncased-emotion', top_k = None)
+        prediction = classifier(summary)
+        emotion = [x['label'] for x in prediction[0]]
+        score = [y['score'] for y in prediction[0]]
 
-            st.session_state['emotion'] = emotion[score.index(np.max(score))]
+        st.session_state['emotion'] = emotion[score.index(np.max(score))]
 
-            fig10 = make_subplots(rows=1, cols=1)
+        fig10 = make_subplots(rows=1, cols=1)
 
-            fig10.add_trace(go.Bar(x = emotion,
-                                    y = score,
-                                    name = f"Dream {st.session_state['row_n']}"))
+        fig10.add_trace(go.Bar(x = emotion,
+                                y = score,
+                                name = f"Dream {st.session_state['row_n']}"))
 
-            fig10.update_layout(
-                                title="Sentiment Classification Results",
-                                xaxis_title="Criteria",
-                                yaxis_title="Sentiment Scores",
-                                legend_title="Dreams"
-                                # font=dict(
-                                #     family="Courier New, monospace",
-                                #     size=18,
-                                #     color="RebeccaPurple"
-                                # )
-                            )    
+        fig10.update_layout(
+                            title="Sentiment Classification Results",
+                            xaxis_title="Criteria",
+                            yaxis_title="Sentiment Scores",
+                            legend_title="Dreams"
+                            # font=dict(
+                            #     family="Courier New, monospace",
+                            #     size=18,
+                            #     color="RebeccaPurple"
+                            # )
+                        )    
 
-            st.plotly_chart(fig10,theme="streamlit", use_container_width=True) 
-            # except Exception as e:
-            #     st.warning("Either OpenAI Key is incorrect or you have chosen inappropriate dream")
-            #     print(e)
-    except:
-        st.warning("Please Complete the Previous Step Before Moving On")
+        st.plotly_chart(fig10,theme="streamlit", use_container_width=True) 
+        # except Exception as e:
+        #     st.warning("Either OpenAI Key is incorrect or you have chosen inappropriate dream")
+        #     print(e)
+    # except:
+    #     st.warning("Please Complete the Previous Step Before Moving On")
 ########################################################################################
 #############################       Dream Summarization + Continuation      #################################
 ######################################################################################## 
