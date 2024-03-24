96c92bd790e729cf1d7d3b985047979c15b6373a
Veo Chae <veochae@gmail.com>
2024-03-24 19:43:03 -0400

    asdf

--- app/app_wip.py
+++ app/app_wip.py
@@ -523,7 +523,7 @@ def data_cleaning():
                     st.session_state['tf_idf_df'],st.session_state['wordcloud_words'] = main(corpus, tokenized)
                     st.write(st.session_state['wordcloud_words'][:10])
 
-                    wordcloud(st.session_state['wordcloud_words'], lim=100)
+                    wordcloud(st.session_state['wordcloud_words'])
                 except Exception as e:
                     st.write(e)
 
